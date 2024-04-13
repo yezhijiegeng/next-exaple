b72cfef94a042c1b0b875255be80d77918f04581
yezhijiegeng <y2474498183@sina.com>
2024-04-13 19:12:33 +0800

    类型错误

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import Sidenav from "@/app/ui/dashboard/sidenav";
 
-export default function Layout({ children }) :{children:React.ReactNode}{
+export default function Layout({ children:any }) :{children:React.ReactNode}{
 	return (
 		<div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
